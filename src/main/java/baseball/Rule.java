f6f8933d4744a897145dc0961716d42452a29a57
youdongkwan <dkyou7@naver.com>
2021-10-05 23:05:54 +0900

    feat: 프로그래밍 제약사항 추가

---
+++ src/main/java/baseball/Rule.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Rule {
+    public static final int SIZE_OF_DIGITS = 3;
+    public final static int MIN_NUMBER = 1;
+    public final static int MAX_NUMBER = 9;
+}
